e7e3f7cfbce3d1bfca291f0729d063cbbc9af80f
zeytov <policimentop@gmail.com>
2026-06-25 16:18:31 +0700

    Update ru.py

--- ru.py
+++ ru.py
@@ -1,1 +1,13 @@
-priv = приветствую тебя в playerok cardinal 
+#приветствие
+hello = приветствую тебя в playerok cardinal
+
+#главное меню
+mm_global = ⚙️глобальные переключатели
+mm_notification = 📣настройка уведомлений
+mm_autoreply = 🤖настройка автоответа
+mm_automated = 🎁настройка авто выдачи
+mm_plugins = 🧩плагины
+mm_response = 📄заготовки ответов
+mm_hello = 👋приветственное сообщение
+mm_confirmation = ✅ответ на подтверждение заказа
+mm_stars = ⭐️ответ на отзывы

29a69b309ce65bd639b45f8a3b1461ba88e899e6
zeytov <policimentop@gmail.com>
2026-06-25 13:22:11 +0300

    Update ru.py

--- ru.py
+++ ru.py
@@ -1,12 +1,25 @@
 #приветствие
 hello = приветствую тебя в playerok cardinal
 
+#нужные
+green = 🟢
+red = 🔴
+
 #главное меню
 mm_global = ⚙️глобальные переключатели
 mm_notification = 📣настройка уведомлений
 mm_autoreply = 🤖настройка автоответа
-mm_automated = 🎁настройка авто выдачи
+mm_automated = 🎁настройка авто-выдачи
 mm_plugins = 🧩плагины
 mm_response = 📄заготовки ответов
 mm_hello = 👋приветственное сообщение
 mm_confirmation = ✅ответ на подтверждение заказа
+mm_config = 📂конфиги
+mm_authorized = 👤авторизованные пользователи
+mm_proxy = 🌐прокси
+
+#глобальные настройки
+gs_autobump = автоподнятие
+gs_delivery = авто-выдача
+gs_answering = автоответчик
+gs_
